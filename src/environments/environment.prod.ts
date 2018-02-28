7d866d5634c921b62cd1e722d4359b9aafe7d908
BenjaminRoth18 <benjamin.roth18@gmail.com>
2018-02-28 21:31:16 +0100

    Added firebase tools

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCmVoguhExDRdUTq69VdMArvCkWudldYkw',
+    authDomain: 'chat-96b27.firebaseapp.com',
+    databaseURL: 'https://chat-96b27.firebaseio.com',
+    projectId: 'chat-96b27',
+    storageBucket: 'chat-96b27.appspot.com',
+    messagingSenderId: '542749835675'
+  }
 };
